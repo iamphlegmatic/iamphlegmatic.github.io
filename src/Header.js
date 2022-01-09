56b7331181010b4b4bbda55a3fb20b051c684618
imphlegmatic <abhijit19058@iiitd.ac.in>
2022-01-10 01:05:33 +0530

    responsive nav-bar

--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,23 @@
 import React from 'react';
+import  { useEffect, useState } from "react";
+import "./style.css";
 
-function MakeVisible(){
-const navilist = document.getElementById("navi-list");
-navilist.classList.toggle("active");
-}
 
 export default function Header(){
+    const [showMenu, setShowMenu] = useState(false);
     return (
-       <nav className="navbar">
+       <nav className={`navbar`}>
           <div className="brand-container">
               <p className="brandname">CODIFY</p>
           </div>
-          <ul className="nav-list" id="navi-list">
+          <ul className={`nav-list ${showMenu && "active"}`}>
               <li><a href="#">Courses</a></li>
               <li><a href="#">Contact</a></li>
               <li><a href="#">Login</a></li>
               <li><a href="#">SignUp</a></li>
               <li><a href="#">Services</a></li>
           </ul>
-          <div className="menu" id="toggle-button" onClick={MakeVisible}>
+          <div className={`menu`}  onClick={() => setShowMenu(!showMenu)}>
                 <div className="menu-item"> </div>
                 <div className="menu-item"> </div>
                 <div className="menu-item"> </div>
